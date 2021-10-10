9d1133550616d4a9a8b5505ed879344138ec0375
FancyFennec <jonasallemann@gmx.ch>
2021-10-10 21:51:45 +0200

    initialised the project

---
+++ Assets/Editor/WaveEditor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Wave))]
+public class WaveEditor : Editor
+{
+
+    private void OnEnable()
+    {
+    }
+
+    private void OnSceneGUI()
+    {
+    }
+
+    public override void OnInspectorGUI()
+    {
+    }
+}
