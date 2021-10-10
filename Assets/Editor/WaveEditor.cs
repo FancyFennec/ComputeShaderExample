0fd05f1b5076916cfc9677202d52103c5fdb28a0
FancyFennec <jonasallemann@gmx.ch>
2021-10-10 22:30:20 +0200

    removed useless files and got the shader to run

--- Assets/Editor/WaveEditor.cs
+++ Assets/Editor/WaveEditor.cs
@@ -6,16 +6,32 @@ using UnityEngine;
 [CustomEditor(typeof(Wave))]
 public class WaveEditor : Editor
 {
-
+    ComputeShader perlinNoise;
+    int perlinNoiseHandle;
+    int resolution = 256;
+    RenderTexture texture;
     private void OnEnable()
     {
+        texture = new RenderTexture(resolution, resolution, 0, RenderTextureFormat.ARGB32)
+        {
+            enableRandomWrite = true,
+        };
+        texture.Create();
+        perlinNoise = (ComputeShader)Resources.Load("PerlinNoise");
+        perlinNoiseHandle = perlinNoise.FindKernel("CSMain");
+        perlinNoise.SetTexture(perlinNoiseHandle, "Result", texture);
+        perlinNoise.Dispatch(perlinNoiseHandle, resolution / 8, resolution / 8, 1);
     }
 
     private void OnSceneGUI()
     {
+        
     }
 
     public override void OnInspectorGUI()
     {
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.ObjectField("Texture", texture, typeof(Texture2D), false);
+        EditorGUILayout.EndHorizontal();
     }
 }
