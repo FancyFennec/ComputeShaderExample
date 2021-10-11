2706cf47ece74503180ee9b98cf855c9a06c04ff
FancyFennec <jonasallemann@gmx.ch>
2021-10-12 00:56:01 +0200

    added basic perlin noise compute shader and vertex displacement

--- Assets/Editor/WaveEditor.cs
+++ Assets/Editor/WaveEditor.cs
@@ -6,21 +6,30 @@ using UnityEngine;
 [CustomEditor(typeof(Wave))]
 public class WaveEditor : Editor
 {
+    Wave wave;
     ComputeShader perlinNoise;
     int perlinNoiseHandle;
     int resolution = 256;
-    RenderTexture texture;
+    public RenderTexture texture;
     private void OnEnable()
     {
-        texture = new RenderTexture(resolution, resolution, 0, RenderTextureFormat.ARGB32)
+        wave = (Wave)target;
+
+        texture = new RenderTexture(resolution, resolution, 0, RenderTextureFormat.RFloat)
         {
-            enableRandomWrite = true,
+            enableRandomWrite = true
         };
         texture.Create();
+
         perlinNoise = (ComputeShader)Resources.Load("PerlinNoise");
         perlinNoiseHandle = perlinNoise.FindKernel("CSMain");
         perlinNoise.SetTexture(perlinNoiseHandle, "Result", texture);
+        perlinNoise.SetFloat("res", (float) resolution);
         perlinNoise.Dispatch(perlinNoiseHandle, resolution / 8, resolution / 8, 1);
+
+		Renderer rend = wave.GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial.SetTexture("_Texture2D", texture);
     }
 
     private void OnSceneGUI()
@@ -31,7 +40,7 @@ public class WaveEditor : Editor
     public override void OnInspectorGUI()
     {
         EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.ObjectField("Texture", texture, typeof(Texture2D), false);
+        EditorGUILayout.ObjectField("Texture", texture, typeof(Texture), false);
         EditorGUILayout.EndHorizontal();
     }
 }
