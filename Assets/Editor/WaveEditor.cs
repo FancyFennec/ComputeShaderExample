5704e0cfaff40130dda84f654e7db7c704c76ef4
FancyFennec <jonasallemann@gmx.ch>
2021-10-12 21:09:51 +0200

    implemented perlin noise shader

--- Assets/Editor/WaveEditor.cs
+++ Assets/Editor/WaveEditor.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -11,31 +12,43 @@ public class WaveEditor : Editor
     int perlinNoiseHandle;
     int resolution = 256;
     public RenderTexture texture;
+
     private void OnEnable()
-    {
-        wave = (Wave)target;
+	{
+		wave = (Wave)target;
 
-        texture = new RenderTexture(resolution, resolution, 0, RenderTextureFormat.RFloat)
-        {
-            enableRandomWrite = true
-        };
-        texture.Create();
+		ComputeBuffer gradients = new ComputeBuffer(256, sizeof(float) * 2);
+		gradients.SetData(Enumerable.Range(0, 256).Select((i) => GetRandomDirection()).ToArray());
 
-        perlinNoise = (ComputeShader)Resources.Load("PerlinNoise");
-        perlinNoiseHandle = perlinNoise.FindKernel("CSMain");
-        perlinNoise.SetTexture(perlinNoiseHandle, "Result", texture);
-        perlinNoise.SetFloat("res", (float) resolution);
-        perlinNoise.Dispatch(perlinNoiseHandle, resolution / 8, resolution / 8, 1);
+		texture = new RenderTexture(resolution, resolution, 0, RenderTextureFormat.RFloat)
+		{
+			enableRandomWrite = true
+		};
+		texture.Create();
+
+		perlinNoise = (ComputeShader)Resources.Load("PerlinNoise");
+		perlinNoiseHandle = perlinNoise.FindKernel("CSMain");
+		perlinNoise.SetTexture(perlinNoiseHandle, "Result", texture);
+		perlinNoise.SetFloat("res", (float)resolution);
+		perlinNoise.SetFloat("t", (float) EditorApplication.timeSinceStartup);
+		perlinNoise.SetBuffer(perlinNoiseHandle, "gradients", gradients);
+		perlinNoise.Dispatch(perlinNoiseHandle, resolution / 8, resolution / 8, 1);
 
 		Renderer rend = wave.GetComponent<Renderer>();
-        rend.enabled = true;
-        rend.sharedMaterial.SetTexture("_Texture2D", texture);
-    }
+		rend.enabled = true;
+		rend.sharedMaterial.SetTexture("_Texture2D", texture);
+	}
 
-    private void OnSceneGUI()
+	private static Vector2 GetRandomDirection()
+	{
+		return new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
+	}
+
+	private void OnSceneGUI()
     {
-        
-    }
+		perlinNoise.SetFloat("t", (float)EditorApplication.timeSinceStartup);
+		perlinNoise.Dispatch(perlinNoiseHandle, resolution / 8, resolution / 8, 1);
+	}
 
     public override void OnInspectorGUI()
     {
